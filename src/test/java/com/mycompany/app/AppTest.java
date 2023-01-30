d56f6e6588e1091261c67da7ef26131f6bd4c482
KhalfaouiAnis <aniskhalfaoui9@gmail.com>
2023-01-30 20:31:21 +0100

    app changes

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!, Welcome to the Devops world!", app.getMessage());
+        assertEquals("Welcome to the Devops world!", app.getMessage());
     }
 }
