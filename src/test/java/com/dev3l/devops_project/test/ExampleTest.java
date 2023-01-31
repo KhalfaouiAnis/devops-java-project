07881e8225e411a50f5cde3f867bacb613f08b07
KhalfaouiAnis <aniskhalfaoui9@gmail.com>
2023-01-31 22:39:02 +0100

    modifie pom.xml

---
+++ src/test/java/com/dev3l/devops_project/test/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.devops.devops_project.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainAppTest {
+	@Test
+	public void mainAppTest() {
+		Assert.assertTrue(true);
+	}
+}
